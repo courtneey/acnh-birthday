92a1d57956b6d5bf73382bf8bde66280b517594e
courtney <rebocourtney@gmail.com>
2021-11-02 15:51:32 -0700

    created initial graphql villager type

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,54 @@
 const router = require("express").Router();
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+} = require("graphql");
+const { db, Villager, Tops } = require("../db/index");
+
+const VillagerType = new GraphQLObjectType({
+  name: "Villager",
+  description: "This represents a villager",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    imageUrl: { type: GraphQLNonNull(GraphQLString) },
+    birthday: { type: GraphQLNonNull(GraphQLString) },
+    style_one: { type: GraphQLNonNull(GraphQLString) },
+    style_two: { type: GraphQLNonNull(GraphQLString) },
+    color_one: { type: GraphQLNonNull(GraphQLString) },
+    color_two: { type: GraphQLNonNull(GraphQLString) },
+  }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root query",
+  fields: () => ({
+    villagers: {
+      type: new GraphQLList(VillagerType),
+      description: "A list of villagers",
+      resolve: () => Villager.findAll(),
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+});
+
+router.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
 
 router.use((req, res, next) => {
   const err = new Error("API route not found!");
