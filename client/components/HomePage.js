c51bf184f05db3255a27effd95243b33ba195093
courtney <rebocourtney@gmail.com>
2021-11-02 13:40:46 -0700

    added submit button to homepage component

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,26 +1,49 @@
 import React, { Component } from "react";
 
 class HomePage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      month: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      month: event.target.value,
+    });
+  }
+
   render() {
+    const { month } = this.state;
+    const { handleChange } = this;
     return (
       <div className="button-div">
         <h2>Select a month:</h2>
-        <p>
-          <select>
-            <option>January</option>
-            <option>February</option>
-            <option>March</option>
-            <option>April</option>
-            <option>May</option>
-            <option>June</option>
-            <option>July</option>
-            <option>August</option>
-            <option>September</option>
-            <option>October</option>
-            <option>November</option>
-            <option>December</option>
+
+        <form id="select-month">
+          <select value={month} onChange={handleChange}>
+            <option value="January">January</option>
+            <option value="February">February</option>
+            <option value="March">March</option>
+            <option value="April">April</option>
+            <option value="May">May</option>
+            <option value="June">June</option>
+            <option value="July">July</option>
+            <option value="August">August</option>
+            <option value="September">September</option>
+            <option value="October">October</option>
+            <option value="November">November</option>
+            <option value="December">December</option>
           </select>
-        </p>
+
+          <div>
+            <button type="submit" className="submit-btn">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
