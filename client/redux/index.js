6c4ca7bb451ca616d8a2432f8abf48226b0386eb
courtney <rebocourtney@gmail.com>
2021-11-03 12:42:57 -0700

    created thunk and reducer for fetching tops by style

--- client/redux/index.js
+++ client/redux/index.js
@@ -3,12 +3,14 @@ import villagersByMonthReducer from "./allVillagers";
 import singleVillagerReducer from "./singleVillager";
 import singleMonthReducer from "./singleMonth";
 import topsByColorReducer from "./topsByColor";
+import topsByStyleReducer from "./topsByStyle";
 
 const appReducer = combineReducers({
   villagersByMonth: villagersByMonthReducer,
   villager: singleVillagerReducer,
   month: singleMonthReducer,
   topsByColor: topsByColorReducer,
+  topsByStyle: topsByStyleReducer,
 });
 
 export default appReducer;
